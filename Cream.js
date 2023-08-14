1099b06cfcd94338e540d12abb27c03501d0c38a
Thomas14Rex <mondal.soumyadeep30@gmail.com>
2023-08-15 00:15:32 +0530

    New Commit

--- Cream.js
+++ Cream.js
@@ -2,7 +2,9 @@ let M= document.getElementById("Weight of Milk");
 let Fm=document.getElementById("Fat in Milk");
 let Fs=document.getElementById("Fat in Skim Milk");
 let Fc=document.getElementById("Fat in Cream");
+let Submit=document.getElementById("Submit");
 let C= M*((Fm-Fs)/(Fc-Fs));
-
-console.log(C);
-console.log("Hello");
+let result= document.getElementById("result")
+Submit.onclick=function(){
+  result.innerHTML=(C);
+}
